28d4671a1b0a3c7a7d49ee8c0611935842b5f2a7
ChronoBoot <146651003+ChronoBoot@users.noreply.github.com>
2023-12-06 18:28:46 +0100

    Add main script. Download dataset (as csv) from an azure container

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from azure.storage.blob import BlobServiceClient
+
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+file_names = [
+    'HomeCredit_columns_description.csv',
+    'POS_CASH_balance.csv',
+    'application_test.csv',
+    'application_train.csv',
+    'bureau.csv',
+    'bureau_balance.csv',
+    'credit_card_balance.csv',
+    'installments_payments.csv',
+    'previous_application.csv',
+    'sample_submission.csv'
+]
+
+
+container_name = 'blob-csv-files'
+download_path = "data/"
+
+container_client = blob_service_client.get_container_client(container=container_name)
+
+for blob_name in file_names:
+    download_file_path = f"{download_path}{blob_name}"
+    blob_client = container_client.get_blob_client(blob_name)
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
