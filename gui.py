4a7b1a7c7d3661fd476089111217426a46cc0328
ChronoBoot <cyprien_g@chronoboot.com>
2023-12-07 17:08:01 +0100

    Correct gui so that the files are loaded before launching the dash server.

--- gui.py
+++ gui.py
@@ -24,6 +24,6 @@ def update_graph(value):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(debug=True, host='0.0.0.0', port=port)
     subprocess.run(["python", "main.py"])
+    app.run_server(debug=True, host='0.0.0.0', port=port)
 
