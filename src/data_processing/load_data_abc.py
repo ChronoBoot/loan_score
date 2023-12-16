e041e0ecc91a264bcd5498188cf1aff49ec5a2ec
ChronoBoot <cyprien_g@chronoboot.com>
2023-12-16 09:11:10 +0100

    Refractoring of data_processing module

--- src/data_processing/load_data_abc.py
+++ src/data_processing/load_data_abc.py
@@ -1,11 +1,40 @@
 from abc import ABC, abstractmethod
+from azure.storage.blob import BlobServiceClient
+from typing import Any, List
 
 class LoadData(ABC):
+    """
+    Abstract base class that defines a contract for loading data from Azure Blob Storage and saving it to a file.
+
+    Any class that inherits from this must implement the `load` and `save` methods.
+    """
 
     @abstractmethod
-    def load(self, blob_service_client, container_name, file_names, download_path):
-       pass
+    def load(self, blob_service_client: BlobServiceClient, container_name: str, file_names: List[str], download_path: str) -> None:
+        """
+        Load data from Azure Blob Storage.
+
+        Args:
+            blob_service_client (BlobServiceClient): The BlobServiceClient instance.
+            container_name (str): The name of the container.
+            file_names (List[str]): The list of file names to download.
+            download_path (str): The local path to download the files.
+
+        Returns:
+            None
+        """
+        pass
 
     @abstractmethod
-    def save(self, file_path, data):
+    def save(self, file_path: str, data: Any) -> None:
+        """
+        Save data to a file.
+
+        Args:
+            file_path (str): The path of the file to save.
+            data (Any): The data to save.
+
+        Returns:
+            None
+        """
         pass
--- src/data_processing/simple_load_data.py
+++ src/data_processing/simple_load_data.py
@@ -1,19 +1,46 @@
 import os
+import logging
 from .load_data_abc import LoadData
 
 class SimpleLoadData(LoadData):
+    """
+    A concrete implementation of the LoadData abstract base class.
+
+    This class provides functionality to load data from Azure Blob Storage and save it to a local directory.
+    The save method is not implemented and should be implemented in a subclass if needed.
+    """
+    
     def load(self, blob_service_client, container_name, file_names, download_path):
-        container_client = blob_service_client.get_container_client(container=container_name)
+        """
+        Load data from Azure Blob Storage and save it to a local directory.
 
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
+        Args:
+            blob_service_client: The BlobServiceClient instance.
+            container_name: The name of the container.
+            file_names: The list of file names to download.
+            download_path: The local path to download the files.
+        """
+        try:
+            container_client = blob_service_client.get_container_client(container=container_name)
 
-        for blob_name in file_names:
-            download_file_path = f"{download_path}{blob_name}"
-            if not os.path.exists(download_file_path):
-                blob_client = container_client.get_blob_client(blob_name)
-                with open(download_file_path, "wb") as download_file:
-                    download_file.write(blob_client.download_blob().readall())
+            if not os.path.exists(download_path):
+                os.makedirs(download_path)
+
+            for blob_name in file_names:
+                download_file_path = f"{download_path}{blob_name}"
+                if not os.path.exists(download_file_path):
+                    blob_client = container_client.get_blob_client(blob_name)
+                    with open(download_file_path, "wb") as download_file:
+                        download_file.write(blob_client.download_blob().readall())
+        except Exception as e:
+            logging.error(f"Failed to load data from Azure Blob Storage: {e}")
 
     def save(self, file_path, data):
-        raise NotImplementedError("The save method is not implemented")
+        """
+        Save data to a file. This method is not implemented in this class and should be implemented in a subclass if needed.
+
+        Args:
+            file_path: The path of the file to save.
+            data: The data to save.
+        """
+        raise NotImplementedError("The save method is not implemented in SimpleLoadData and should be implemented in a subclass if needed.")
